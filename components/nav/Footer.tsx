532d9a66c40c17377805724377b557c72f744042
konst-teknik <information@konst-teknik.se>
2025-04-11 17:28:12 +0200

    Fix

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -5,7 +5,7 @@ import { apiQuery } from 'next-dato-utils/api';
 export default async function Footer() {
 	return <footer className={s.footer}>
 
-		<div>Gävleborg Art Guide drivs av&nbsp;<a href="https://gavleborg.konstframjandet.se/">Konstfrämjandet Gävleborg</a>, och har startat upp med stöd av Region Gävleborg och&nbsp;<a href="https://www.konstframjandet.se/">Folkrörelsernas Konstfrämjande</a>.</div>
+		<div>Gävleborg Art Guide drivs av&nbsp;<a href="https://gavleborg.konstframjandet.se/">Konstfrämjandet Gävleborg</a>, och har startat upp med stöd av Region Gävleborg och&nbsp;<a href="https://www.konstframjandet.se/">Folkrörelsernas Konstfrämjande</a>. Vill du att ditt evenemang ska synas här? <a href="mailto:gavleborg@konstframjandet.se">Kontakta oss</a>! </div>
 		<div className={s.contact}><a href="https://www.facebook.com/konstframjandetgavleborg">Facebook</a> <a href="https://www.instagram.com/konstframjandetgavleborg/">Instagram</a> <a href="mailto:gavleborg@konstframjandet.se">Kontakt</a></div>
 	</footer>;
 }
